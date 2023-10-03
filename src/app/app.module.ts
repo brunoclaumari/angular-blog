e0276c45d2575ac7fb4ada395d1078f62167d6ab
brunoclaumari <brunoclaumari@gmail.com>
2023-10-02 23:48:55 -0300

    Ajustes no big-card e main-header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialHeaderComponent } from './components/social-header/social-header.component';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialHeaderComponent,
+    MainHeaderComponent,
+    BigCardComponent,
+    CarouselComponent,
+    SmallCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
