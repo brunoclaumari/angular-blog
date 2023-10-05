f4ad20ef6b7f95d7ee4db16198d4b45bcdd8ee02
brunoclaumari <brunoclaumari@gmail.com>
2023-10-04 22:30:36 -0300

    Adicionando propriedades dinâmicas aos componentes

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent implements OnInit {
+  /*
 
-  constructor() { }
+<p class="category">Saladas e entradas</p>
+    <p class="title" >Salada Primavera Simples</p>
+    <p class="data" >02/10/2023 - 3 min de leitura</p>
+    */
+  @Input()
+  category: string = 'Saladas e Entradas';
 
-  ngOnInit(): void {
-  }
+  @Input()
+  title: string = 'Salada Primavera Simples';
 
+  @Input()
+  data: string = '02/10/2023 - 3 min de leitura';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+
+  @Input()
+  link_image:string='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuJvXmeUX4XVM1b7VJ_GzdzibbHG99fQL-WEjMGLlpll-CM1qxRbaFNCuAC6jEtpRMMjY&usqp=CAU';
+
+  @Input()
+  featured:string = "Lembrança da vovó";
+
+  @Input()
+  recipe_title:string = "Sopa de feijão";
+
+  @Input()
+  recipe_data:string = "03 de Outubro de 2023";
+
+  @Input()
+  recipe_category:string = "Sopas";
 
   constructor() { }
 
